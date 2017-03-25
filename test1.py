023a062dc0ad01d4f83ea318dfabd6b736ae6a20
SunnerLi <a6214123@gmail.com>
2017-03-25 17:46:55 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+from template import template
+
+THRESHOLD = 5000
+
+def templateMatching(img):
+    # Get the size of the image
+    img_height = np.shape(img)[0]
+    img_width = np.shape(img)[1]
+
+    # Transfer the result as three channel
+    result_show = np.ndarray([img_height, img_width, 3])
+    for i in range(img_height):
+        for j in range(img_width):
+            result_show[i][j] = img[i][j]
+
+    template_num = np.shape(template)[0]
+    for k in range(template_num):
+        # Get the size of the mask
+        mask_height = np.shape(template[k])[0]
+        mask_width = np.shape(template[k])[1]
+
+        # Get the stride of the mask
+        slide_y = img_height - mask_height + 1
+        slide_x = img_width - mask_width + 1
+
+        # Do the template matching
+        result = np.zeros([slide_y, slide_x])
+        for i in range(slide_y):
+            for j in range(slide_x):
+                roi = img[i:i+mask_height, j:j+mask_width]
+                result[i][j] = np.sum(roi * template[k])
+
+        # Do the binary thresholding
+        for i in range(np.shape(result)[0]):
+            for j in range(np.shape(result)[1]):
+                if result[i][j] >= THRESHOLD:
+                    result[i][j] = 1
+                else:
+                    result[i][j] = 0
+
+        # Graw the result
+        for i in range(np.shape(result)[0]):
+            for j in range(np.shape(result)[1]):
+                if result[i][j] == 1:
+                    result_show[i+1][j+1]=[255, 0, 0]
+    return result_show
+
+if __name__ == "__main__":
+    img = cv2.imread('cornerImg1.png', 0)
+    cv2.imwrite('result.png', templateMatching(img))
